d827bd43d5d51d945ed987b456c18536e04b1f38
ashleyhrv2 <168749264+ashleyhrv2@users.noreply.github.com>
2026-03-02 08:01:02 -0500

    Add files via upload

---
+++ Restaurant Ordering System/src/javaPack/DisplayMenu.java
@@ -0,0 +1,18 @@
+package javaPack;
+
+public class DisplayMenu 
+{
+	public DisplayMenu()
+	{
+		String title = "Welcome to the Restaurant \n";
+		String line = "----------------------------\n\n";
+		
+		String menu = "Menu:\n";
+		menu += "\t1. Breakfast \n ";
+		menu += "\t1. Breakfast \n ";
+	}
+	
+	
+	
+
+}
---
+++ Restaurant Ordering System/src/javaPack/Main.java
@@ -0,0 +1,12 @@
+package javaPack;                                            
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		DisplayMenu dspmenu = new DisplayMenu();
+
+	}
+
+}
